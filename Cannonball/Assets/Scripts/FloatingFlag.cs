3bc77d80ffa4c80c9a184095882ec32f15f160b7
DizzyIzzy <DizzyIzzy@users.noreply.github.com>
2018-03-14 10:11:32 -0700

    Initial Upload from HD

---
+++ Cannonball/Assets/Scripts/FloatingFlag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingFlag : MonoBehaviour {
+    public Text flagText;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 flagPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        flagText.transform.position = flagPos;
+	}
+}
