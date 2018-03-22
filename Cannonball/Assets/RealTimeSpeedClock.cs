c9b8b15eed12e88346e098b30e3675989d149f93
DizzyIzzy <DizzyIzzy@users.noreply.github.com>
2018-03-21 19:58:03 -0700

    Added improved elapsed clock, with no rollback and real calendar days.
    Still need to connect scheduleBuilder to the new gameclock system

---
+++ Cannonball/Assets/RealTimeSpeedClock.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RealTimeSpeedClock : MonoBehaviour {
+    public DateTime startDateTime;
+    public DateTime gameDateTime;
+
+    public float startTimeFloat;
+    public float elapsedTime;
+    public float timeFactor;
+
+    public string gameDateString;
+    public TimeSpan simElapsed;
+    public string simElapsedString;
+
+    private GUIStyle guiStyle = new GUIStyle();
+
+    // Use this for initialization
+    void Start () {
+        startDateTime = DateTime.Now;
+        startTimeFloat = Time.time;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float timeIncrement = (Time.deltaTime) * timeFactor;
+        elapsedTime = elapsedTime + timeIncrement;
+        double elapseDouble = elapsedTime;
+       
+        gameDateTime = startDateTime.AddSeconds(elapseDouble);
+        gameDateString = gameDateTime.ToString();
+        simElapsed = gameDateTime.Subtract(startDateTime);
+        simElapsedString = simElapsed.ToString();
+
+    }
+
+    void OnGUI()
+    {
+        guiStyle.fontSize = 20;
+        GUI.Label(new Rect(20, 50, 1000, 20), ("GameDay: " + gameDateString));
+        GUI.Label(new Rect(20, 80, 1000, 20), ("SimDay: "  + simElapsed.TotalDays.ToString("00") + "/SimRunning: "+ simElapsedString));
+
+    }
+}
--- Cannonball/Assets/Scripts/CanadianRhythm.cs
+++ Cannonball/Assets/Scripts/CanadianRhythm.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,17 +7,23 @@ using UnityEngine.UI;
 public class CanadianRhythm : MonoBehaviour {
     private float startTime;
     public float elapsedTime;
+    public DateTime realTimeticks;
+    public float startTicks;
+    public float elapsedTicks;
+
 
     public static float timeScale;
     public static int calendarDay;
     public static int dayOfWeek;
     public static float timeOfDay;
+    
 
     private GUIStyle guiStyle = new GUIStyle();
     // Use this for initialization
     private void Awake()
     {
         startTime = Time.time;
+        startTicks = DateTime.Now.Ticks;
     }
     void Start () {
         calendarDay=1;
@@ -30,6 +37,8 @@ public class CanadianRhythm : MonoBehaviour {
         timeOfDay = (elapsedTime*timeScale) % (24 ) ;
         calendarDay = Mathf.CeilToInt((elapsedTime * timeScale) / 24);
         dayOfWeek = calendarDay % 7;
+
+    
     }
 
     void OnGUI() {
